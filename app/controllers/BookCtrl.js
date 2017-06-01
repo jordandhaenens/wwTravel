9722f4f0e5db856fe5d61c0cd9a3b4aa7e545faf
Jordan Dhaenens <jordan.dhaenens@gmail.com>
2017-06-01 13:16:27 -0500

    cleaned up the code a bit. Filter is still not working

--- app/controllers/BookCtrl.js
+++ app/controllers/BookCtrl.js
@@ -3,13 +3,13 @@ console.log("bookCtrl.js is loaded");
 
 
 app.controller('BookCtrl', function($scope, GuideFactory){
-	$scope.something = "";
-	$scope.getVal = function(el){
-		console.log("$scope.something", $scope.something);
-		$scope.something = el;
-	};
-	console.log("$scope.something in controller", $scope.something);
-	// console.log('controller is firing');
+	// $scope.something = "";
+	// $scope.getVal = function(el){
+	// 	console.log("el", el);
+	// 	return el;
+	// 	// $scope.something = el;
+	// };
+	// console.log("$scope.something in controller", $scope.something);
 	GuideFactory.getBooks()
 	.then(function(data){
 		console.log('data in controller', data);
